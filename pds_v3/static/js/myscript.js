177fca763659c86030b3dc903b1db74402f38120
Alexander1994 <Alex.McCallum>
2016-05-20 10:38:47 -0300

    detail page cleaned up

--- pds_v3/static/js/myscript.js
+++ pds_v3/static/js/myscript.js
@@ -8,22 +8,8 @@ $(document).ready(function() {
 
 function loadXMLDoc(url,div, direct_to)
 {
-    // Changes url to have flag set incase user refreshes
-
-    var xmlhttp;
-    //url = 'https://www.pdsquirrel.ca';
-    url = 'http://127.0.0.1/' + url;
-    console.log('url used: ' + url);
-
-    xmlhttp = new XMLHttpRequest();
-    //url = 'https://www.pdsquirrel.ca//user/presenter/dash/?myaccount';
-    xmlhttp.onreadystatechange = function(){
-	if (xmlhttp.readyState==4) {
-	    $(div).html(xmlhttp.responseText);
-	}
-    }
-    xmlhttp.open("GET",url,true);
-    //xmlhttp.setRequestHeader('Access-Control-Allow-Origin', 'https://www.pdsquirrel.ca');
-    xmlhttp.send();
+		$.get(url, function( data ) {
+ 			$(div).html( data );
+ 		});
     window.history.pushState("state", direct_to, "?direct_to=" + direct_to);
 }
---
+++ pds_v3/static/js/user/detail.js
@@ -0,0 +1,95 @@
+
+// stripe
+
+    Stripe.setPublishableKey('pk_test_URIzgmhYoVYuzDm9Q98RHkIQ');
+    jQuery(function($) {
+      $('#payment-form').submit(function(event) {
+        var $form = $(this);
+
+		if (document.getElementById('e_card').checked == true) {
+			event.preventDefault();
+			$form.find('button').prop('disabled', true);
+			console.log('existing card used');
+			$.ajax({
+				type: "POST",
+				url: "{% url 'payment-process' %}",
+				data: $form.serialize(),
+			}).done(function(data) {
+				$form.append(data);
+			}).fail(function(data) {
+			console.log('failed');
+			});
+			return false;
+		}
+		if (document.getElementById('n_card').checked == true) {
+			// Disable the submit button to prevent repeated clicks
+			$form.find('button').prop('disabled', true);
+
+			Stripe.card.createToken($form, stripeResponseHandler);
+
+			// Prevent the form from Sessiange pn with the default action
+			return false;
+		}
+      });
+    });
+
+    function stripeResponseHandler(status, response) {
+      var $form = $('#payment-form');
+
+      if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').text(response.error.message);
+        $form.find('button').prop('disabled', false);
+      } else {
+        // response contains id and card, which contains additional card details
+        var token = response.id;
+		console.log(token);
+        // Insert the token into the form so it gets submitted to the server
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+        // and submit
+		$.ajax({
+			type: "POST",
+			url: "{% url 'payment-process' %}",
+			data: $form.serialize(),
+		}).done(function(data) {
+			$form.append(data);
+		}).fail(function(data) {
+			console.log('failed');
+		});
+      }
+    };
+
+// radio button toggling
+
+$(document).ready(function() {
+
+	var $new_radio = document.getElementById('n_card');
+	var $existing_radio = document.getElementById('e_card');
+	var $cancel_btn_one = document.getElementById('cancel_btn_one');
+
+	$('#existing_card_col :input').prop('disabled', true);
+	$('#new_card_col :input').prop('disabled', true);
+
+
+	$cancel_btn_one.onclick = function(e) {
+		e.preventDefault();
+		$('#payment-modal').modal('hide');
+		$('#existing_card_col :input').val('');
+		$('#new_card_col :input').val('');
+	}
+
+	$new_radio.onclick = function() {
+		$existing_radio.checked = false;
+		$('#existing_card_col :input').prop('disabled', true);
+		$('#new_card_col :input').prop('disabled', false);
+		$('#existing_card_col :input').val('');
+	}
+	$existing_radio.onclick = function() {
+		$new_radio.checked = false;
+		$('#new_card_col :input').prop('disabled', true);
+		$('#existing_card_col :input').prop('disabled', false);
+		$('#new_card_col :input').val('');
+
+	}
+});
