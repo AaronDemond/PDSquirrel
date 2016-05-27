0253aef19cd3f6813859527604fad688b738fa11
Alexander1994 <Alex.McCallum>
2016-05-27 12:22:26 -0300

    cleaned up leftover js in join

--- pds_v3/static/js/user/join.js
+++ pds_v3/static/js/user/join.js
@@ -13,7 +13,6 @@ $('input, select').focusout(function() {
   var curr_val = $curr.val();
   var error_occured = true;               // error assumed happen for concise code
   var error_text = "";                    // text defining the error
-  console.log($curr);
 
   if(curr_val=="") {
     if ($curr.prop('tagName') =='SELECT')
@@ -32,7 +31,7 @@ $('input, select').focusout(function() {
 
   } else if (curr_id == "vpass" && curr_val != $('#pass').val()) {
     error_text = "Your password does not match";
-    
+
   } else { // no errors found
     error_occured = false;
   }
@@ -63,7 +62,6 @@ $('input, select').focusout(function() {
       vpass.parent("div").addClass("has-success has-feedback");
       vpass.next('.help-block').text(error_text);
     }
-    console.log(curr_id);
     $parent.removeClass("has-error");
     $parent.addClass("has-success has-feedback");
   }
@@ -77,74 +75,3 @@ $('#joinform').submit(function() {
     return false;
   }
 });
-
-//$('#joinform').submit(function() {
-// error handler for submitting the join form
-/*
-  $('#term-help').remove();
-  $('#page-alerts').empty();
-  $('#page-alerts').addClass("hidden");
-  var error = 0;
-  var p_error = 0;
-  // text_input is div holding inputs that contain text inputs
-
-  */
-  //return false;
-  /*
-  try {
-    for (var text_field in text_input) {
-      if (text_input.hasOwnProperty(text_field)) {
-        if (text_input[text_field].val()==="") {
-          text_input[text_field]
-        }
-      }
-    }
-  } catch (e) {
-    console.log(e);
-  } finally {
-    return false;
-  }
-*/
-
-   /*
-  if ( $('#email').val() == "" || $('#first_name').val() == "" || $('#last_name').val() == "" || $('#email').val() == "" || $('#id_captcha_0').val() == "" || $('#termbox').is(':checked') == 0) {
-    $('#example').click();
-    error++;
-  }
-  if ( $('#termbox').is(':checked') == 0 ) {
-    $('#termdiv').append("<p class=\"bg-danger\" id = \"term-help\" style=\"background-color: white; font-size: 1.2em; color: rgb(169, 68, 66); font-weight: bold\" c>You must accept the terms and conditions to use PD Squirrel</p>");
-  }
-
-  $('.help-block').remove();
-  $('.form-group').removeClass('has-error');
-
-  if ( $('#email').val() != $('#vemail').val() ) {
-    $('#emailgroup').addClass("has-error");
-    $('#vemailgroup').addClass("has-error");
-    $('#emailgroup').append("<span id=\"emailhelp\" class=\"help-block\">Emails do not match</span>");
-    $('#vemailgroup').append("<span id=\"vemailhelp\" class=\"help-block\">Emails do not match</span>");
-    error++;
-  }
-  if ( $('#pass').val() != $('#vpass').val() ) {
-    $('#passgroup').addClass("has-error");
-    $('vpassgroup').addClass("has-error");
-    $('#passgroup').append("<span id=\"passmatchhelp\" class=\"help-block\">Passwords do not match</span>");
-    $('#vpassgroup').append("<span class=\"help-block\">Passwords do not match</span>");
-    p_error++;
-    error++;
-  }
-
-  if ( $('#pass').val().length < 8 ) {
-    p_error++;
-    error++;
-    $('#passgroup').addClass("has-error");
-    $('#passgroup').append("<span id=\"passlenhelp\" class=\"help-block\">Password must be over 8 characters long</span>");
-  }
-
-  if (error > 0) {
-     return false;
-  } else {
-      return true;
-  }*/
-
-//});
