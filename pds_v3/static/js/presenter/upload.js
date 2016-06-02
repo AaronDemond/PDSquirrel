d5fc447849338264be36560bde812d897f7f6541
AaronDemond <aarondemond@dal.ca>
2016-06-02 11:31:12 -0300

    Finished js/css restructuring on pres hub

---
+++ pds_v3/static/js/presenter/upload.js
@@ -0,0 +1,56 @@
+function showInfo(infolink){
+	row_id = "#info-row";
+		$(row_id).toggleClass("hidden");
+	if ($(infolink).text() == '(info ▾)'){
+		$(infolink).text('(info ▴)');
+	}
+	else {
+		$(infolink).text('(info ▾)');
+	}
+}
+
+function closeInfo() {
+	$('#info-row').addClass('hidden');
+	$('#infoLink').text('(info ▾)');
+}
+
+
+/*	file counter gets increased with each upload, resets on page reload.
+	adds a div with a label and new file input box to form
+	server loops through files
+*/
+
+var file_counter = 1;
+function addAttachment(){
+	if (file_counter < 5) {
+		file_counter++;
+		file_counter_str = file_counter.toString();
+		file_id = '#file-upload-'.concat(file_counter_str)
+		var new_div = '<div class="control-group"><label for="">Attachment #'.concat(file_counter_str).concat('</label><input type="file" class="form-control" name="').concat(file_id).concat('" ></div>');
+		$('#file-upload-container').append(new_div);
+	} else {
+		alert("You have added the maximum number of additional file fields to the form");
+	}
+}
+
+function anotherFile() {
+	$('#addfile').removeClass('hidden');
+}
+
+$("form").submit(function() {
+	$('#loader').removeClass('hidden');
+	$('fieldset').addClass('hidden');
+	});
+$('#online').click(function () {
+
+	$('#online_input').prop('disabled', false);
+	var upload_input = $('#upload_input')
+	upload_input.prop('disabled', true);
+	upload_input.val('');
+});
+$('#upload').click(function() {
+	$('#upload_input').prop('disabled', false);
+	var online_input = $('#online_input');
+	online_input.prop('disabled', true);
+	online_input.val('');
+});
