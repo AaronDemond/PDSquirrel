fb904c33a4ff8f77127b07893a23a5dc02486477
AaronDemond <aarondemond@dal.ca>
2016-07-08 14:23:14 -0300

    Added admin appuser tasks
    Can now create a presenter and send activation email
    from administration

--- pds_v3/admin.py
+++ pds_v3/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
 from pds_v3.models import PdSession, AppUser, LawSociety, \
-LawSocietyOverride, Purchase, Subject, PdSessionEdit, Notice, PdAttachment, Address, PdAudio, Comment
+LawSocietyOverride, Purchase, Subject, PdSessionEdit, Notice, PdAttachment, Address, PdAudio, Comment, Presenter
 from pds_v3 import models
+import tasks
 
 admin.site.site_header = 'PD Squirrel Administration'
 
 
+def make_presenter(modeladmin, request, queryset):
+    for appuser in queryset:
+        appuser.is_presenter = True
+        appuser.save()
+        p = Presenter(user=appuser.user)
+        p.save()
+
+
+
+def send_activation(modeladmin, request, queryset):
+    for appuser in queryset:
+        msg = "Hello " + appuser.user.first_name + " " + appuser.user.last_name + ", and welcome to PD Squirrel!\n\nPlease click on the following link and use your email address to activate your membership account: https://pdsquirrel.ca/user/activate/%s\n\nThanks,\nThe PD Squirrel admin team" % (str(appuser.activation_key) + "/")
+        subject = 'PD Squirrel Activation'
+        send_to = [appuser.user.email, 'demondsoftware@gmail.com', 'cdemond@cwdlaw.ca']
+        tasks.sendMail.apply_async([send_to, subject, msg])
+        
+
 @admin.register(AppUser)
 class AppUserAdmin(admin.ModelAdmin):
     list_filter = ('is_presenter','is_premium')
+    actions = [send_activation, make_presenter]
+
+
 
 admin.site.register(PdSession)
 admin.site.register(PdAudio)
