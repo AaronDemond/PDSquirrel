e09d7c554de04a65bacbe0a8730c5fdb43d48e45
Alexander1994 <Alex.McCallum>
2016-08-03 11:24:48 -0300

    myscripts properly labeled as loadxmldoc

--- pds_v3/static/js/shared/loadxmldoc.js
+++ pds_v3/static/js/shared/loadxmldoc.js
@@ -1,11 +1,4 @@
 
-$(document).ready(function() {
-	$('#pres-hub-nav li').click(function() {
-	$("#pres-hub-nav li").removeClass('active');
-	$(this).addClass('active');
-    });
-});
-
 function loadXMLDoc(url,div, direct_to) {
 	$.ajax( {
 		type: "GET",
