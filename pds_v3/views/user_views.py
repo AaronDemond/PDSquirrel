5194d0b95caec0b9cd57683fb61d3ca7251342c1
Alexander1994 <Alex.McCallum>
2016-08-17 13:39:50 -0300

    Revert "Added redirect to activation to avoid csrf issue"
    This reverts commit 11810a95b81987c60365581b8d5646a94f79ee5b.

--- pds_v3/views/user_views.py
+++ pds_v3/views/user_views.py
@@ -36,7 +36,7 @@ def activate(request, link_id):
     except:
         messages.error(request, 'Account not found. Please check the link for errors.')
 
-    return HttpResponseRedirect('/login/')
+    return render(request, 'v3/final/login-new.html')
 
 def login_user(request):
     username = request.POST['username']
